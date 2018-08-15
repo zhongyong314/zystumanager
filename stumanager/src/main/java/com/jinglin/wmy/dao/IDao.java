92faf645223f8e707dc8c3197ac1dcfaaa4c3b40
DELL <DELL@DESKTOP-GDO5RDF>
2018-08-15 09:42:38 +0800

    这是版本2.0

--- stumanager/src/main/java/com/jinglin/wmy/dao/IDao.java
+++ stumanager/src/main/java/com/jinglin/wmy/dao/IDao.java
@@ -2,6 +2,12 @@ package com.jinglin.wmy.dao;
 
 import java.util.List;
 
+/**
+ * 数据库访问的接口
+ * @author DELL
+ *
+ * @param <T>
+ */
 public interface IDao<T> {
    int additem(T t);
    int updateitem(T t);
